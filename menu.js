3f78a49337af0838bf1fe3706f1b768ffdcddd10
Mauro <maurofigueroa.dev@gmail.com>
2022-06-26 22:00:18 -0300

    Agregando validación al formulario y algunos fix visuales

--- menu.js
+++ menu.js
@@ -2,10 +2,20 @@ var imgH = document.querySelector(".logo-menuH");
 var imgX = document.querySelector(".logo-close");
 var menuHamburguesa = document.querySelector(".menu-hamburguesa");
 var html = document.querySelector("html");
+var linkInicio = document.querySelector(".menu-inicio");
+var linkSobreMi = document.querySelector(".menu-sobre-mi");
+var linkHabilidades = document.querySelector(".menu-habilidades");
+var linkPortafolio = document.querySelector(".menu-portafolio");
+var linkContacto = document.querySelector(".menu-contacto");
 
 
 imgH.onclick = mostrarMenuH;
 imgX.onclick = quitarMenuH;
+linkInicio.onclick = quitarMenuH;
+linkSobreMi.onclick = quitarMenuH;
+linkHabilidades.onclick = quitarMenuH;
+linkPortafolio.onclick = quitarMenuH;
+linkContacto.onclick = quitarMenuH;
 
 function mostrarMenuH(){
     /*menuHamburguesa.classList.remove("oculto");*/
@@ -24,3 +34,4 @@ function quitarMenuH(){
     imgX.classList.add("oculto")
     html.classList.remove("overflowH");
 }
+
